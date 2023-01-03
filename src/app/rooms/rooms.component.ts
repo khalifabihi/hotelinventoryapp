b994dafc0f19063bfefb2dea9710a807a76d3416
Khalifa <khalifa.bihi351@campus.vinci.ac.ma>
2023-01-03 20:40:17 +0100

    Hotel Inventory App

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent implements OnInit {
+
+  hotelName = "Hilton Hotel";
+  numberOfRooms = 10;
+  hotelService = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggle() {
+    this.hotelService = !this.hotelService;
+  }
+
+}
