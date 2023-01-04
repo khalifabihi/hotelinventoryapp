e2c08bb2a75c86d354ddeccd78e3b3155ceb62cf
Khalifa <khalifa.bihi351@campus.vinci.ac.ma>
2023-01-04 19:33:48 +0100

    Hotel Inventory App

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,23 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './room';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent implements OnInit {
-
-  hotelName = "Hilton Hotel";
+  hotelName = 'Hilton Hotel';
   numberOfRooms = 10;
-  hotelService = false;
+  hideRooms = false;
 
-  constructor() { }
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
 
-  ngOnInit(): void {
-  }
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe',
+      amenities: 'AC, TV, Wifi',
+      price: 1000,
+      photos: 'https://picsum.photos/200/300',
+      checkInTime: new Date('11-Jan-2023'),
+      checkOutTime: new Date('12-Jan-2023'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Super Deluxe',
+      amenities: 'AC, TV, Wifi, Fridge',
+      price: 2000,
+      photos: 'https://picsum.photos/200/300',
+      checkInTime: new Date('11-Jan-2023'),
+      checkOutTime: new Date('12-Jan-2023'),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Luxury',
+      amenities: 'AC, TV, Wifi, Fridge, Pool',
+      price: 3000,
+      photos: 'https://picsum.photos/200/300',
+      checkInTime: new Date('11-Jan-2023'),
+      checkOutTime: new Date('12-Jan-2023'),
+    }
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
   toggle() {
-    this.hotelService = !this.hotelService;
+    this.hideRooms = !this.hideRooms;
   }
-
 }
