cc7e860d0a85be99392e644f27789e2cf81a01e5
Khalifa <khalifa.bihi351@campus.vinci.ac.ma>
2023-01-05 20:28:50 +0100

    Hotel App Management

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RoomList } from '../room';
+
+@Component({
+  selector: 'app-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.scss']
+})
+export class RoomsListComponent implements OnInit {
+
+  @Input() rooms: RoomList[] = [];
+
+  @Output() selectedRoom = new EventEmitter<RoomList>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom.emit(room);
+  }
+
+}
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -11,47 +11,55 @@ export class RoomsComponent implements OnInit {
   numberOfRooms = 10;
   hideRooms = false;
 
+  selectedRoom!: RoomList;
+
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe',
-      amenities: 'AC, TV, Wifi',
-      price: 1000,
-      photos: 'https://picsum.photos/200/300',
-      checkInTime: new Date('11-Jan-2023'),
-      checkOutTime: new Date('12-Jan-2023'),
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Super Deluxe',
-      amenities: 'AC, TV, Wifi, Fridge',
-      price: 2000,
-      photos: 'https://picsum.photos/200/300',
-      checkInTime: new Date('11-Jan-2023'),
-      checkOutTime: new Date('12-Jan-2023'),
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Luxury',
-      amenities: 'AC, TV, Wifi, Fridge, Pool',
-      price: 3000,
-      photos: 'https://picsum.photos/200/300',
-      checkInTime: new Date('11-Jan-2023'),
-      checkOutTime: new Date('12-Jan-2023'),
-    }
-  ];
+  roomList: RoomList[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe',
+        amenities: 'AC, TV, Wifi',
+        price: 1000,
+        photos: 'https://picsum.photos/200/300',
+        checkInTime: new Date('11-Jan-2023'),
+        checkOutTime: new Date('12-Jan-2023'),
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Super Deluxe',
+        amenities: 'AC, TV, Wifi, Fridge',
+        price: 2000,
+        photos: 'https://picsum.photos/200/300',
+        checkInTime: new Date('11-Jan-2023'),
+        checkOutTime: new Date('12-Jan-2023'),
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Luxury',
+        amenities: 'AC, TV, Wifi, Fridge, Pool',
+        price: 3000,
+        photos: 'https://picsum.photos/200/300',
+        checkInTime: new Date('11-Jan-2023'),
+        checkOutTime: new Date('12-Jan-2023'),
+      }
+    ];
+  }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
   }
 }
